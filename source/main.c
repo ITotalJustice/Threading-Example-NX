4679a033ed9f5f4693a306be6b0b822bde57ee12
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2020-01-05 11:02:37 +0000

    Remove console print

--- source/main.c
+++ source/main.c
@@ -22,10 +22,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#include <switch.h>
 #include <threads.h>
 
 
@@ -46,12 +44,10 @@ typedef struct
 } thread_t;
 
 /*
-*   We create 2 mutex's that will be locked.
+*   We create a mutex that will be locked.
 *   The file mtx is for the read / write functions.
-*   The console mtx is for when we want to write to stdout and update the console.  
 */
 mtx_t file_mtx;
-mtx_t console_mtx;
 
 /*
 *   Our conditional vars.
@@ -74,25 +70,9 @@ size_t get_file_size(const char *file)
     return size;
 }
 
-//  Thread safe console update function.
-void print_console(const char *text, ...)
-{
-    mtx_lock(&console_mtx);
-
-    va_list v;
-    va_start(v, text);
-    vfprintf(stdout, text, v);
-    va_end(v);
-    consoleUpdate(NULL);
-
-    mtx_unlock(&console_mtx);
-}
-
 //  The read thread function.
 int thrd_read(void *in)
 {
-    print_console("inside read function\n\n");
-
     thread_t *t = (thread_t *)in;
     if(!t) return -1;
 
@@ -106,30 +86,21 @@ int thrd_read(void *in)
         if (done + bufsize > t->total_size)
             bufsize = t->total_size - done;
 
-
-        print_console("reading into buffer\n\n");
         fread(buf, bufsize, 1, fp);
 
-        print_console("locking mutex in read\n\n");
         mtx_lock(&file_mtx);
-        print_console("locked mutex in read\n\n");
         if (t->data_size != 0)
         {
-            print_console("waiting for can read cond\n\n");
             cnd_wait(&can_read, &file_mtx);
-            print_console("got can read cond\n\n");
         }
 
-        print_console("copying data in read\n\n");
         memcpy(t->data, buf, bufsize);
         t->data_size = bufsize;
 
         mtx_unlock(&file_mtx);
         cnd_signal(&can_write);
-        print_console("unlocked read mutex and sent can write cond\n\n");
     }
 
-    print_console("exiting read thread\n\n");
     free(buf);
     fclose(fp);
     return 0;
@@ -138,8 +109,6 @@ int thrd_read(void *in)
 //  The write thread function.
 int thrd_write(void *in)
 {
-    print_console("inside write function\n\n");
-
     thread_t *t = (thread_t *)in;
     if (!t) return 1;
 
@@ -148,38 +117,27 @@ int thrd_write(void *in)
 
     while (t->data_written < t->total_size)
     {
-        print_console("locking write mutex\n\n");
         mtx_lock(&file_mtx);
-        print_console("locked write mutex\n\n");
-
         if (t->data_size == 0)
         {
-            print_console("waiting for can write cond\n\n");
             cnd_wait(&can_write, &file_mtx);
-            print_console("got can write cond\n\n");
         }
 
-        print_console("writing data to file %lu\n\n", t->data_size);
         fwrite(t->data, t->data_size, 1, fp);
         t->data_written += t->data_size;
         t->data_size = 0;
-        print_console("currently written %lu - %lu\n\n", t->data_written, t->total_size);
 
         mtx_unlock(&file_mtx);
         cnd_signal(&can_read);
-        print_console("unlocked write mutex and sent can read cond\n\n");
     }
 
-    print_console("exiting write thread\n\n");
     fclose(fp);
     return 0;
 }
 
 bool init_app(void)
 {
-    if (!consoleInit(NULL)) return false;
     if (!mtx_init(&file_mtx, mtx_plain)) return false;
-    if (!mtx_init(&console_mtx, mtx_plain)) return false;
     if (!cnd_init(&can_read)) return false;
     if (!cnd_init(&can_write)) return false;
     return true;
@@ -187,11 +145,9 @@ bool init_app(void)
 
 void exit_app(void)
 {
-    consoleExit(NULL);
     cnd_destroy(&can_read);
     cnd_destroy(&can_write);
     mtx_destroy(&file_mtx);
-    mtx_destroy(&console_mtx);
 }
 
 int main(int argc, char *argv[])
@@ -201,15 +157,12 @@ int main(int argc, char *argv[])
         goto jmp_exit;
     }
 
-    print_console("getting file size\n\n");
     size_t file_size = get_file_size(INFILE);
     if (!file_size)
     {
         goto jmp_exit;
     }
-    print_console("got file size\n\n");
 
-    print_console("setting up struct %s", "test\n\n");
     thread_t thread_struct;
     thread_struct.data = malloc(BUFSIZE);
     thread_struct.data_size = 0;
@@ -219,11 +172,9 @@ int main(int argc, char *argv[])
     thrd_t t_read;
     thrd_t t_write;
 
-    print_console("creating threads\n\n");
     thrd_create(&t_read, thrd_read, &thread_struct);
     thrd_create(&t_write, thrd_write, &thread_struct);
 
-    print_console("waiting for threads to join\n\n");
     thrd_join(t_read, NULL);
     thrd_join(t_write, NULL);
     free(thread_struct.data);
